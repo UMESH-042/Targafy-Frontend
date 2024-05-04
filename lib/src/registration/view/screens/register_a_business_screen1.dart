2e6ed888b8aefed599a057b64b14a6b1dd88e931
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-05 00:38:56 +0530

    removed unused imports

--- lib/src/registration/view/screens/register_a_business_screen1.dart
+++ lib/src/registration/view/screens/register_a_business_screen1.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:targafy/core/constants/colors.dart';
 import 'package:targafy/core/constants/dimensions.dart';
