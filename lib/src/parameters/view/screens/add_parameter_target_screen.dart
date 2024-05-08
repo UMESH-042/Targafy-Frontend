e2b060da604772f1c35f73e9cebc0af7499f0704
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-09 00:30:53 +0530

    created parameter and target tiles

--- lib/src/parameters/view/screens/add_parameter_target_screen.dart
+++ lib/src/parameters/view/screens/add_parameter_target_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:targafy/core/constants/colors.dart';
 import 'package:targafy/core/constants/dimensions.dart';
 import 'package:targafy/core/utils/texts.dart';
+import 'package:targafy/src/parameters/view/widgets/parameter_tile.dart';
+import 'package:targafy/src/parameters/view/widgets/target_tile.dart';
 
 class AddParameterTargetScreen extends StatefulWidget {
   const AddParameterTargetScreen({super.key});
@@ -48,14 +51,14 @@ class _AddParameterTargetScreenState extends State<AddParameterTargetScreen> wit
                   });
                 },
                 child: AnimatedContainer(
-                   alignment: Alignment.center,
+                    alignment: Alignment.center,
                     duration: const Duration(milliseconds: 200),
                     decoration: BoxDecoration(
-                      border: Border.all(color: primaryColor, width: 1 ),
+                      border: Border.all(color: primaryColor, width: 1),
                       color: selectedValue == 1 ? primaryColor : Colors.white,
                       borderRadius: BorderRadius.circular(8).copyWith(topRight: const Radius.circular(0), bottomRight: const Radius.circular(0)),
                     ),
-                         width: getScreenWidth(context) * 0.3,
+                    width: getScreenWidth(context) * 0.3,
                     child: CustomText(
                       text: 'Parameters',
                       fontSize: getScreenWidth(context) * 0.04,
@@ -70,11 +73,10 @@ class _AddParameterTargetScreenState extends State<AddParameterTargetScreen> wit
                   });
                 },
                 child: AnimatedContainer(
-                  
-                  alignment: Alignment.center,
+                    alignment: Alignment.center,
                     duration: const Duration(milliseconds: 200),
                     decoration: BoxDecoration(
-                      border: Border.all(color: primaryColor, width: 1 ),
+                      border: Border.all(color: primaryColor, width: 1),
                       color: selectedValue == 2 ? primaryColor : Colors.white,
                       borderRadius: BorderRadius.circular(8).copyWith(topLeft: const Radius.circular(0), bottomLeft: const Radius.circular(0)),
                     ),
@@ -87,6 +89,32 @@ class _AddParameterTargetScreenState extends State<AddParameterTargetScreen> wit
                     )),
               ),
             ],
+          ),
+          if(selectedValue ==1)
+          SizedBox(
+            height: getScreenheight(context) * 0.6,
+            child: ListView(
+              children: const [
+                ParameterTile(parameterName: 'Sales', userAssigned: 5),
+                ParameterTile(parameterName: 'Items Sold', userAssigned: 10),
+                ParameterTile(parameterName: 'Revenue', userAssigned: 78),
+                ParameterTile(parameterName: 'Secondary Sales', userAssigned: 15),
+                ParameterTile(parameterName: 'Margins', userAssigned: 1),
+              ],
+            ),
+          ),
+          if(selectedValue ==2)
+          SizedBox(
+            height: getScreenheight(context) * 0.6,
+            child: ListView(
+              children: const [
+                TargetTIle(parameterName: 'Sales', target: 150),
+                TargetTIle(parameterName: 'Items Sold', target: 100),
+                TargetTIle(parameterName: 'Revenue', target: 10),
+                TargetTIle(parameterName: 'Secondary Sales', target: 180),
+                TargetTIle(parameterName: 'Margins', target: 200),
+              ],
+            ),
           ),
         ],
       ),
---
+++ lib/src/parameters/view/widgets/parameter_tile.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:targafy/core/constants/colors.dart';
+import 'package:targafy/core/constants/dimensions.dart';
+import 'package:targafy/core/utils/texts.dart';
+
+class ParameterTile extends StatelessWidget {
+  const ParameterTile({
+    super.key,
+    required this.parameterName,
+    required this.userAssigned
+  });
+
+  final String parameterName;
+  final int userAssigned;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: primaryColor, width: 2),
+      ),
+      margin: EdgeInsets.symmetric(horizontal: getScreenWidth(context) * 0.06, vertical: getScreenheight(context) * 0.02).copyWith(top: 0),
+      padding: EdgeInsets.symmetric(horizontal: getScreenWidth(context) * 0.04, vertical: getScreenheight(context) * 0.005).copyWith(right: 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Image.asset('assets/img/graph.png'),
+              SizedBox(width: getScreenWidth(context) * 0.04),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(
+                    text: parameterName,
+                    fontSize: getScreenWidth(context) * 0.04,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,
+                  ),
+                  CustomText(
+                    text: "User's assigned : $userAssigned",
+                    fontSize: getScreenWidth(context) * 0.03,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.chevron_right,
+                  color: primaryColor,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
