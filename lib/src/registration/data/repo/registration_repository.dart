84c737afb9a215b47da1984ebe91e9b312262c90
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-05-09 03:28:26 +0530

    written apis for login and verify otp

---
+++ lib/src/registration/data/repo/registration_repository.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:targafy/core/constants/url.dart';
+import 'package:targafy/core/utils/print_log.dart';
+
+class RegistrationRepo {
+  static Future<Map<String, dynamic>> login({required String countryCode, required String phone}) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      body: jsonEncode({
+        "contactNumber": {"countryCode": countryCode, "number": phone}
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      PrintLog.printLog(response.body);
+      throw Exception('Failed to login');
+    }
+  }
+
+  static Future<Map<String, dynamic>> verifyOtp({required String countryCode, required String phone, required String otp}) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      body: jsonEncode({
+        "otp": otp,
+        "contactNumber": {"countryCode": countryCode, "number": phone}
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      PrintLog.printLog(response.body);
+      return jsonDecode(response.body);
+    } else {
+      PrintLog.printLog(response.body);
+      throw Exception('Failed to login');
+    }
+  }
+}
