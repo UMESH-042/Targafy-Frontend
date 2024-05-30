4887b2a93a0d88c9264e32b73ce114a6e73eb778
testgithubtiwari <rt936649@gmail.com>
2024-05-30 19:16:59 +0530

    merge from umesh

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ void main() async {
   bool hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
   final expiryTime = prefs.getInt('expiryTime') ?? 0;
   final isTokenValid = DateTime.now().millisecondsSinceEpoch < expiryTime;
-  print(token);
+  // print(token);
   runApp(ProviderScope(
       child: MyApp(
     hasSeenOnboarding: hasSeenOnboarding,
