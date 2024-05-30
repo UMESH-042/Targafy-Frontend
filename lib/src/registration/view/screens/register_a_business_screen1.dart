99de7a4e74ee747b482d71a0aa90cce7a76e71ca
testgithubtiwari <rt936649@gmail.com>
2024-05-30 20:48:38 +0530

    all merge done

--- lib/src/registration/view/screens/register_a_business_screen1.dart
+++ lib/src/registration/view/screens/register_a_business_screen1.dart
@@ -1,22 +1,23 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:targafy/core/constants/colors.dart';
 import 'package:targafy/core/constants/dimensions.dart';
 import 'package:targafy/core/shared/components/primary_button.dart';
 import 'package:targafy/src/auth/view/Controllers/login.dart';
 import 'package:targafy/src/home/view/screens/widgets/Bottom_navigation_bar.dart';
 import 'package:targafy/src/registration/view/screens/register_a_business_screen2.dart';
-import 'package:http/http.dart' as http;
 
 class RegisterABusinessScreen1 extends ConsumerStatefulWidget {
   const RegisterABusinessScreen1({super.key});
 
   @override
-  State<RegisterABusinessScreen1> createState() => _RegisterABusinessScreen1State();
+  ConsumerState<RegisterABusinessScreen1> createState() =>
+      _RegisterABusinessScreen1State();
 }
 
-class _RegisterABusinessScreen1State extends State<RegisterABusinessScreen1> {
+class _RegisterABusinessScreen1State
+    extends ConsumerState<RegisterABusinessScreen1> {
   @override
   Widget build(BuildContext context) {
     final loginState = ref.watch(loginProvider);
@@ -25,8 +26,10 @@ class _RegisterABusinessScreen1State extends State<RegisterABusinessScreen1> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: getScreenheight(context)*0.2,),
-            
+            SizedBox(
+              height: getScreenheight(context) * 0.2,
+            ),
+
             Container(
               margin: EdgeInsets.all(getScreenWidth(context) * 0.05),
               decoration: BoxDecoration(
@@ -172,12 +175,39 @@ class _RegisterABusinessScreen1State extends State<RegisterABusinessScreen1> {
             //       text: 'Proceed'),
             // )
             Container(
-              margin: EdgeInsets.symmetric(horizontal: getScreenWidth(context) * 0.05).copyWith(top: getScreenheight(context) * 0.05),
+              margin: EdgeInsets.symmetric(
+                horizontal: getScreenWidth(context) * 0.05,
+              ).copyWith(
+                top: getScreenheight(context) * 0.05,
+              ),
               child: PrimaryButton(
-                  function: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterABusinessScreen2()));
-                  },
-                  text: 'Proceed'),
+                function: () async {
+                  // Call the function to check business existence
+                  final exists = await loginNotifier.checkBusinessExists();
+
+                  if (exists) {
+                    // Business already exists
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Business already exists')),
+                    );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const BottomNavigationAndAppBar()),
+                    );
+                  } else {
+                    // Navigate to the next screen if business does not exist
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const RegisterABusinessScreen2()),
+                    );
+                  }
+                },
+                text: 'Proceed',
+              ),
             )
           ],
         ),
