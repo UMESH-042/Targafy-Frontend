f2b37a2b27d6dcaaa82bd87859b58c0a49a4b619
UMESH-042 <202151042@iiitvadodara.ac.in>
2024-06-01 16:49:27 +0530

    Made Changes

--- lib/src/auth/view/screens/verify_login_otp.dart
+++ lib/src/auth/view/screens/verify_login_otp.dart
@@ -78,138 +78,20 @@
 // //   }
 // // }
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
-// import 'package:targafy/core/constants/colors.dart';
-// import 'package:targafy/core/constants/dimensions.dart';
-// import 'package:targafy/core/shared/components/primary_button.dart';
-// import 'package:targafy/src/auth/view/Controllers/login.dart';
-// import 'package:targafy/src/home/view/screens/Mandatory_filed.dart';
-// import 'package:targafy/src/registration/view/screens/register_a_business_screen1.dart';
-// import 'package:targafy/utils/utils.dart';
-
-// class VerifyOTPScreen extends ConsumerStatefulWidget {
-//   const VerifyOTPScreen({super.key});
-
-//   @override
-//   ConsumerState<VerifyOTPScreen> createState() => _VerifyOTPScreenState();
-// }
-
-// class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
-//   String otp = "";
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final loginNotifier = ref.read(loginProvider.notifier);
-
-//     return Scaffold(
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             GestureDetector(
-//               onTap: () {
-//                 Navigator.pop(context);
-//               },
-//               child: Container(
-//                 margin: EdgeInsets.symmetric(
-//                     horizontal: getScreenWidth(context) * 0.04,
-//                     vertical: getScreenheight(context) * 0.04),
-//                 alignment: Alignment.centerLeft,
-//                 child: Image.asset('assets/img/back.png'),
-//               ),
-//             ),
-//             Container(
-//               alignment: Alignment.center,
-//               child: Image.asset('assets/img/verify_screen.png'),
-//             ),
-//             Text(
-//               'OTP Verification',
-//               style: TextStyle(
-//                 fontFamily: 'Sofia Pro',
-//                 fontWeight: FontWeight.w700,
-//                 fontSize: getScreenWidth(context) * 0.06,
-//               ),
-//             ),
-//             Text(
-//               'Manage Your Business Easily With Us',
-//               style: TextStyle(
-//                   fontFamily: 'Sofia Pro',
-//                   fontWeight: FontWeight.w400,
-//                   fontSize: getScreenWidth(context) * 0.04,
-//                   color: tertiaryColor),
-//             ),
-//             SizedBox(height: getScreenheight(context) * 0.05),
-//             OtpTextField(
-//               autoFocus: true,
-//               cursorColor: Colors.black,
-//               fieldWidth: getScreenWidth(context) * 0.12,
-//               borderRadius: BorderRadius.circular(12),
-//               textStyle: const TextStyle(
-//                   fontWeight: FontWeight.bold, color: Colors.black),
-//               showCursor: false,
-//               borderWidth: 2,
-//               numberOfFields: 4,
-//               borderColor: primaryColor,
-//               showFieldAsBox: true,
-//               onSubmit: (String code) {
-//                 setState(() {
-//                   otp = code;
-//                 });
-//               },
-//               disabledBorderColor: primaryColor,
-//               enabledBorderColor: primaryColor,
-//             ),
-//             Container(
-//               alignment: Alignment.centerLeft,
-//               margin: EdgeInsets.symmetric(
-//                   horizontal: getScreenWidth(context) * 0.05),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   SizedBox(height: getScreenheight(context) * 0.02),
-//                   SizedBox(height: getScreenheight(context) * 0.05),
-//                   PrimaryButton(
-//                     function: () async {
-//                       loginNotifier.updateOtpCode(otp);
-//                       final success =
-//                           await loginNotifier.verifyLoginOtp(context);
-//                       if (success) {
-//                         // Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterABusinessScreen1()));
-//                         Navigator.pushReplacement(
-//                             context,
-//                             MaterialPageRoute(
-//                                 builder: (context) => MandatoryFieldPage()));
-//                       } else {
-//                         showSnackBar(context, 'Invalid OTP. Please try again.',
-//                             Colors.red);
-//                       }
-//                     },
-//                     text: 'Verify OTP',
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:targafy/core/constants/colors.dart';
 import 'package:targafy/core/constants/dimensions.dart';
 import 'package:targafy/core/shared/components/primary_button.dart';
+import 'package:targafy/src/auth/view/Controllers/login.dart';
 import 'package:targafy/src/home/view/screens/Mandatory_filed.dart';
 import 'package:targafy/src/home/view/screens/controller/mandatory_Filed_name_controller.dart';
 import 'package:targafy/src/registration/view/screens/register_a_business_screen1.dart';
 import 'package:targafy/utils/utils.dart';
 
 class VerifyOTPScreen extends ConsumerStatefulWidget {
-  const VerifyOTPScreen({Key? key}) : super(key: key);
+  const VerifyOTPScreen({super.key});
 
   @override
   ConsumerState<VerifyOTPScreen> createState() => _VerifyOTPScreenState();
@@ -217,18 +99,20 @@ class VerifyOTPScreen extends ConsumerStatefulWidget {
 
 class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
 
-  
-  @override
+
+    @override
   void initState() {
     super.initState();
     ref.read(nameControllerProvider.notifier).checkFirstTime();
   }
+  
   String otp = "";
 
   @override
   Widget build(BuildContext context) {
-
+    final loginNotifier = ref.read(loginProvider.notifier);
      final state = ref.watch(nameControllerProvider);
+
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -261,11 +145,10 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
             Text(
               'Manage Your Business Easily With Us',
               style: TextStyle(
-                fontFamily: 'Sofia Pro',
-                fontWeight: FontWeight.w400,
-                fontSize: getScreenWidth(context) * 0.04,
-                color: tertiaryColor,
-              ),
+                  fontFamily: 'Sofia Pro',
+                  fontWeight: FontWeight.w400,
+                  fontSize: getScreenWidth(context) * 0.04,
+                  color: tertiaryColor),
             ),
             SizedBox(height: getScreenheight(context) * 0.05),
             OtpTextField(
@@ -274,9 +157,7 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
               fieldWidth: getScreenWidth(context) * 0.12,
               borderRadius: BorderRadius.circular(12),
               textStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
+                  fontWeight: FontWeight.bold, color: Colors.black),
               showCursor: false,
               borderWidth: 2,
               numberOfFields: 4,
@@ -293,8 +174,7 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
             Container(
               alignment: Alignment.centerLeft,
               margin: EdgeInsets.symmetric(
-                horizontal: getScreenWidth(context) * 0.05,
-              ),
+                  horizontal: getScreenWidth(context) * 0.05),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -302,16 +182,25 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
                   SizedBox(height: getScreenheight(context) * 0.05),
                   PrimaryButton(
                     function: () async {
-                   
+                      loginNotifier.updateOtpCode(otp);
+                      final success =
+                          await loginNotifier.verifyLoginOtp(context);
+                      if (success) {
 
-                      // if (state.isFirstTime) {
-                      //   Navigator.pushReplacement(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //       builder: (context) => MandatoryFieldPage(),
-                      //     ),
-                      //   );
-                      // } else {
+                        // // Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterABusinessScreen1()));
+                        // Navigator.pushReplacement(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => MandatoryFieldPage()));
+                        
+                      if (state.isFirstTime) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MandatoryFieldPage(),
+                          ),
+                        );
+                      } else {
                         Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
@@ -319,7 +208,11 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
                                 RegisterABusinessScreen1(),
                           ),
                         );
-                      // }
+                      }
+                      } else {
+                        showSnackBar(context, 'Invalid OTP. Please try again.',
+                            Colors.red);
+                      }
                     },
                     text: 'Verify OTP',
                   ),
@@ -332,3 +225,139 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+// import 'package:targafy/core/constants/colors.dart';
+// import 'package:targafy/core/constants/dimensions.dart';
+// import 'package:targafy/core/shared/components/primary_button.dart';
+// import 'package:targafy/src/home/view/screens/Mandatory_filed.dart';
+// import 'package:targafy/src/home/view/screens/controller/mandatory_Filed_name_controller.dart';
+// import 'package:targafy/src/registration/view/screens/register_a_business_screen1.dart';
+// import 'package:targafy/utils/utils.dart';
+
+// class VerifyOTPScreen extends ConsumerStatefulWidget {
+//   const VerifyOTPScreen({Key? key}) : super(key: key);
+
+//   @override
+//   ConsumerState<VerifyOTPScreen> createState() => _VerifyOTPScreenState();
+// }
+
+// class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> {
+
+  
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   ref.read(nameControllerProvider.notifier).checkFirstTime();
+  // }
+//   String otp = "";
+
+//   @override
+//   Widget build(BuildContext context) {
+
+    //  final state = ref.watch(nameControllerProvider);
+
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             GestureDetector(
+//               onTap: () {
+//                 Navigator.pop(context);
+//               },
+//               child: Container(
+//                 margin: EdgeInsets.symmetric(
+//                     horizontal: getScreenWidth(context) * 0.04,
+//                     vertical: getScreenheight(context) * 0.04),
+//                 alignment: Alignment.centerLeft,
+//                 child: Image.asset('assets/img/back.png'),
+//               ),
+//             ),
+//             Container(
+//               alignment: Alignment.center,
+//               child: Image.asset('assets/img/verify_screen.png'),
+//             ),
+//             Text(
+//               'OTP Verification',
+//               style: TextStyle(
+//                 fontFamily: 'Sofia Pro',
+//                 fontWeight: FontWeight.w700,
+//                 fontSize: getScreenWidth(context) * 0.06,
+//               ),
+//             ),
+//             Text(
+//               'Manage Your Business Easily With Us',
+//               style: TextStyle(
+//                 fontFamily: 'Sofia Pro',
+//                 fontWeight: FontWeight.w400,
+//                 fontSize: getScreenWidth(context) * 0.04,
+//                 color: tertiaryColor,
+//               ),
+//             ),
+//             SizedBox(height: getScreenheight(context) * 0.05),
+//             OtpTextField(
+//               autoFocus: true,
+//               cursorColor: Colors.black,
+//               fieldWidth: getScreenWidth(context) * 0.12,
+//               borderRadius: BorderRadius.circular(12),
+//               textStyle: const TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.black,
+//               ),
+//               showCursor: false,
+//               borderWidth: 2,
+//               numberOfFields: 4,
+//               borderColor: primaryColor,
+//               showFieldAsBox: true,
+//               onSubmit: (String code) {
+//                 setState(() {
+//                   otp = code;
+//                 });
+//               },
+//               disabledBorderColor: primaryColor,
+//               enabledBorderColor: primaryColor,
+//             ),
+//             Container(
+//               alignment: Alignment.centerLeft,
+//               margin: EdgeInsets.symmetric(
+//                 horizontal: getScreenWidth(context) * 0.05,
+//               ),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   SizedBox(height: getScreenheight(context) * 0.02),
+//                   SizedBox(height: getScreenheight(context) * 0.05),
+//                   PrimaryButton(
+//                     function: () async {
+                   
+
+                      // // if (state.isFirstTime) {
+                      // //   Navigator.pushReplacement(
+                      // //     context,
+                      // //     MaterialPageRoute(
+                      // //       builder: (context) => MandatoryFieldPage(),
+                      // //     ),
+                      // //   );
+                      // // } else {
+                      //   Navigator.pushReplacement(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //       builder: (context) =>
+                      //           RegisterABusinessScreen1(),
+                      //     ),
+                      //   );
+                      // // }
+//                     },
+//                     text: 'Verify OTP',
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
