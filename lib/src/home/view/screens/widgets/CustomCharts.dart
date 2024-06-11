9ae1e28377bde375c8734f79f9c1a12ebfda09c6
UMESH-042 <202151042@iiitvadodara.ac.in>
2024-06-11 11:43:04 +0530

    Initialized the graph with the (0,0)

--- lib/src/home/view/screens/widgets/CustomCharts.dart
+++ lib/src/home/view/screens/widgets/CustomCharts.dart
@@ -1,3 +1,56 @@
+// import 'package:flutter/material.dart';
+// import 'package:syncfusion_flutter_charts/charts.dart';
+
+// class CustomChart extends StatelessWidget {
+//   final String parameter;
+//   final List<List<dynamic>> actualData;
+//   final List<List<dynamic>> predictedData;
+
+//   const CustomChart({super.key,
+//     required this.parameter,
+//     required this.actualData,
+//     required this.predictedData,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SfCartesianChart(
+//       primaryXAxis: const CategoryAxis(
+//         title: AxisTitle(
+//           text: 'Months',
+//           textStyle: TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//       ),
+//       primaryYAxis: NumericAxis(
+//         title: AxisTitle(
+//           text: parameter,
+//           textStyle: const TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//       ),
+//       title: ChartTitle(text: '$parameter Analysis'),
+//       legend: const Legend(isVisible: true),
+//       tooltipBehavior: TooltipBehavior(enable: true),
+//       series: <LineSeries<dynamic, String>>[ // Change here
+//         LineSeries<dynamic, String>(
+//           dataSource: actualData,
+//           xValueMapper: (data, _) => data[0].toString(),
+//           yValueMapper: (data, _) => double.parse(data[1].toString()),
+//           name: 'Actual $parameter',
+//           dataLabelSettings: const DataLabelSettings(isVisible: true),
+//           color: Colors.red,
+//         ),
+//         LineSeries<dynamic, String>(
+//           dataSource: predictedData,
+//           xValueMapper: (data, _) => data[0].toString(),
+//           yValueMapper: (data, _) => double.parse(data[1].toString()),
+//           name: 'Predicted $parameter',
+//           dataLabelSettings: const DataLabelSettings(isVisible: true),
+//           color: Colors.blue,
+//         )
+//       ],
+//     );
+//   }
+// }
 
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
@@ -7,7 +60,8 @@ class CustomChart extends StatelessWidget {
   final List<List<dynamic>> actualData;
   final List<List<dynamic>> predictedData;
 
-  const CustomChart({super.key, 
+  const CustomChart({
+    super.key,
     required this.parameter,
     required this.actualData,
     required this.predictedData,
@@ -16,22 +70,24 @@ class CustomChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SfCartesianChart(
-      primaryXAxis: const CategoryAxis(
+      primaryXAxis: CategoryAxis(
         title: AxisTitle(
           text: 'Months',
-          textStyle: TextStyle(fontWeight: FontWeight.bold),
+          textStyle: const TextStyle(fontWeight: FontWeight.bold),
         ),
+        minimum: 0, // Ensure the X-axis starts at 0
       ),
       primaryYAxis: NumericAxis(
         title: AxisTitle(
           text: parameter,
           textStyle: const TextStyle(fontWeight: FontWeight.bold),
         ),
+        minimum: 0, // Ensure the Y-axis starts at 0
       ),
       title: ChartTitle(text: '$parameter Analysis'),
       legend: const Legend(isVisible: true),
       tooltipBehavior: TooltipBehavior(enable: true),
-      series: <LineSeries<dynamic, String>>[ // Change here
+      series: <LineSeries<dynamic, String>>[
         LineSeries<dynamic, String>(
           dataSource: actualData,
           xValueMapper: (data, _) => data[0].toString(),
