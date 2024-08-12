660dc3a1ba5070f6c8c94c524f017da99c0e4d4c
UMESH-042 <202151042@iiitvadodara.ac.in>
2024-08-13 03:42:03 +0530

    resolved small bug

--- lib/src/groups/ui/groups_screen.dart
+++ lib/src/groups/ui/groups_screen.dart
@@ -74,6 +74,123 @@
 //     );
 //   }
 // }
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:lottie/lottie.dart';
+// import 'package:targafy/business_home_page/controller/business_controller.dart';
+// import 'package:targafy/core/constants/colors.dart';
+// import 'package:targafy/src/groups/ui/Create_group.dart';
+// import 'package:targafy/src/groups/ui/widget/group_tile.dart';
+// import 'package:targafy/src/home/view/screens/controller/Department_controller.dart';
+// import 'package:targafy/src/home/view/screens/controller/user_role_controller.dart'; // Import the department provider
+
+// class GroupScreen extends ConsumerStatefulWidget {
+//   const GroupScreen({super.key});
+
+//   @override
+//   _GroupScreenState createState() => _GroupScreenState();
+// }
+
+// class _GroupScreenState extends ConsumerState<GroupScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Consumer(
+//       builder: (context, ref, _) {
+//         final userRoleAsyncValue = ref.watch(userRoleProvider);
+
+//         return userRoleAsyncValue.when(
+//           data: (role) {
+//             if (role == 'User' || role == 'MiniAdmin') {
+//               return Scaffold(
+//                 body: Center(
+//                   child: Column(
+//                     mainAxisSize: MainAxisSize.min,
+//                     children: [
+//                       Lottie.asset('assets/animations/empty_list.json',
+//                           height: 200, width: 200),
+//                       const Text(
+//                         'You don\'t have access to this page',
+//                         style: TextStyle(
+//                           color: Colors.grey,
+//                           fontSize: 14,
+//                           fontWeight: FontWeight.w600,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               );
+//             }
+
+//             final selectedBusinessData = ref.watch(currentBusinessProvider);
+//             final businessId = selectedBusinessData?['business']?.id;
+
+//             if (businessId == null) {
+//               return const Scaffold(
+//                 body: Center(
+//                   child: Text('No business selected'),
+//                 ),
+//               );
+//             }
+
+//             // Watch the departmentProvider with the businessId
+//             final departmentAsyncValue =
+//                 ref.watch(departmentProvider(businessId));
+
+//             return departmentAsyncValue.when(
+//               data: (departments) {
+//                 if (departments.isEmpty) {
+//                   return const Center(
+//                     child: Text(
+//                       'No departments created',
+//                       style: TextStyle(fontSize: 18, color: Colors.red),
+//                     ),
+//                   );
+//                 }
+
+//                 return ListView.builder(
+//                   itemCount: departments.length,
+//                   itemBuilder: (context, index) {
+//                     final department = departments[index];
+//                     return DepartmentTile(department: department);
+//                   },
+//                 );
+//               },
+//               loading: () => const Center(child: CircularProgressIndicator()),
+//               error: (error, stack) => Center(
+//                 child: Column(
+//                   mainAxisSize: MainAxisSize.min,
+//                   children: [
+//                     Lottie.asset('assets/animations/empty_list.json',
+//                         height: 200, width: 200),
+//                     const Text(
+//                       "Nothing to display",
+//                       style: TextStyle(
+//                         color: Colors.grey,
+//                         fontSize: 14,
+//                         fontWeight: FontWeight.w600,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             );
+//           },
+
+//           loading: () => const Scaffold(
+//             body: Center(child: CircularProgressIndicator()),
+//           ),
+//           error: (error, stack) => Scaffold(
+//             body: Center(
+//               child: Text('Failed to load user role: $error'),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lottie/lottie.dart';
@@ -137,42 +254,66 @@ class _GroupScreenState extends ConsumerState<GroupScreen> {
             final departmentAsyncValue =
                 ref.watch(departmentProvider(businessId));
 
-            return departmentAsyncValue.when(
-              data: (departments) {
-                if (departments.isEmpty) {
-                  return const Center(
-                    child: Text(
-                      'No departments created',
-                      style: TextStyle(fontSize: 18, color: Colors.red),
+            return Scaffold(
+              body: departmentAsyncValue.when(
+                data: (departments) {
+                  if (departments.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        'No departments created',
+                        style: TextStyle(fontSize: 18, color: Colors.red),
+                      ),
+                    );
+                  }
+
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: ListView.builder(
+                      itemCount: departments.length,
+                      itemBuilder: (context, index) {
+                        final department = departments[index];
+                        return DepartmentTile(department: department);
+                      },
                     ),
                   );
-                }
-
-                return ListView.builder(
-                  itemCount: departments.length,
-                  itemBuilder: (context, index) {
-                    final department = departments[index];
-                    return DepartmentTile(department: department);
-                  },
-                );
-              },
-              loading: () => const Center(child: CircularProgressIndicator()),
-              error: (error, stack) => Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Lottie.asset('assets/animations/empty_list.json',
-                        height: 200, width: 200),
-                    const Text(
-                      "Nothing to display",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
+                },
+                loading: () => const Center(child: CircularProgressIndicator()),
+                error: (error, stack) => Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Lottie.asset('assets/animations/empty_list.json',
+                          height: 200, width: 200),
+                      const Text(
+                        "Nothing to display",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                backgroundColor: lightblue,
+                onPressed: () async {
+                  final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DepartmentCreatePage()),
+                  );
+
+                  final selectedBusinessData =
+                      ref.read(currentBusinessProvider);
+                  final businessId = selectedBusinessData?['business']?.id;
+
+                  if (result == true && businessId != null) {
+                    ref.invalidate(departmentProvider(businessId));
+                  }
+                },
+                child: const Icon(Icons.add),
               ),
             );
           },
