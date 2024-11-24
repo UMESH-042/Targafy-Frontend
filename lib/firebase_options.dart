f6250f09d05402b3580b28b5e1109e02b2604977
UMESH-042 <202151042@iiitvadodara.ac.in>
2024-11-24 11:27:54 +0530

    added sensitive files in gitignore

---
+++ lib/firebase_options.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: 'AIzaSyDOvzfkKSpxpcAdmfC21tNJA93uDDKttew',
+  projectId: 'targafy-288b2',
+  messagingSenderId: '669034374306',
+  appId: '1:669034374306:android:1813bdc955cbecf305a3fb',
+);
