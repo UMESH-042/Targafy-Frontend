7d405c6952ae79d0ec2a596d7be05bbdfca5905c
UMESH-042 <202151042@iiitvadodara.ac.in>
2024-11-24 11:20:50 +0530

    first commit

--- lib/firebase_options.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-
-const FirebaseOptions firebaseOptions = FirebaseOptions(
-  apiKey: 'AIzaSyDOvzfkKSpxpcAdmfC21tNJA93uDDKttew',
-  projectId: 'targafy-288b2',
-  messagingSenderId: '669034374306',
-  appId: '1:669034374306:android:1813bdc955cbecf305a3fb',
-);
